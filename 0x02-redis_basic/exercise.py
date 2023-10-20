812ef61ce5a37916bb6a64bb91a73a17fa3499b9
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-20 15:32:57 +0300

    redis setup

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" A module that contains a function that stores an item in Redis. """
+
+import redis
+from uuid import uuid4
+
+
+class Cache:
+    """ Implementing a Cache class. """
+
+    def __init__(self):
+        """ Initialize. """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: bytes | str | int | float) -> str:
+        """ Takes a data argument and returns a string. """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
