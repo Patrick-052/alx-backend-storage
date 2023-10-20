e1b350416fa92c480a67bc57e50e54c3e8f8da6b
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-20 16:45:33 +0300

    Decoding

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -19,3 +19,27 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ Takes a key string argument and an optional Callable argument
+        and returns the converted data. """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Automatically parametrize Cache.get to str. """
+        dstr = self._redis.get(key)
+        return dstr.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """ Automatically parametrize Cache.get to int. """
+        data = self._redis.get(key)
+        try:
+            dint = int(data.decode('utf-8'))
+        except Exception:
+            dint = 0
+        return dint
