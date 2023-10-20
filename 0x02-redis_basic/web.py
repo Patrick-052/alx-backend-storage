3a7d20ea871882c54c8abea439e3d4032ce460ca
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-20 21:00:30 +0300

    Establishing a time-limited web cache and monitor

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,20 +2,19 @@
 """ Module that contains implementation of scraping
     and returning the results """
 
-import requests
 import redis
+import requests
 from functools import wraps
-from typing import Callable
+
+r = redis.Redis()
 
 
 def count_requests(method):
     """ Count the number a request is made """
-
     @wraps(method)
     def wrapper(url):
         """ wrapper function to count the number
         of requests with key count as an expiry key """
-        r = redis.Redis()
         cached = f"cached:{url}"
         if r.get(cached):
             return r.get(cached).decode('utf-8')
