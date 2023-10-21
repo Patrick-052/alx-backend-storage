b2aff8f3ec52f1ed86ce0af13d9a7bee649565df
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-21 04:41:10 +0300

    annotations

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,10 +10,10 @@ from typing import Callable
 r = redis.Redis()
 
 
-def count_requests(method: Callable[[str], str]) -> Callable[[str], str]:
+def count_requests(get_page: Callable[[str], str]) -> Callable[[str], str]:
     """ Count the number a request is made """
 
-    @wraps(method)
+    @wraps(get_page)
     def wrapper(url):
         """ wrapper function to count the number
         of requests with key count as an expiry key """
@@ -22,7 +22,7 @@ def count_requests(method: Callable[[str], str]) -> Callable[[str], str]:
             return r.get(cached).decode('utf-8')
 
         count = f"count:{url}"
-        page = method(url)
+        page = get_page(url)
 
         r.incr(count)
         r.setex(cached, 10, page)
