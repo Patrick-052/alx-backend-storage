44d9eee8836eb80cbd4df5791c6d2d74b8b8a2ab
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-21 04:16:53 +0300

    annotations

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,6 +11,7 @@ from typing import Callable
 
 def access_count(get_page: Callable[[str], str]) -> Callable[[str], str]:
     """Track calls to `get_page` with a particular url """
+
     @wraps(get_page)
     def wrapper(url: str) -> str:
         key = f"count:{url}"
@@ -18,6 +19,7 @@ def access_count(get_page: Callable[[str], str]) -> Callable[[str], str]:
         store.incr(key)
         store.expire(key, 10)
         return get_page(url)
+
     return wrapper
 
 
