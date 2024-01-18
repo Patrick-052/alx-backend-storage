0bb80202b6a0bb14114e3050e542eea92dcde60c
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-19 02:28:11 +0300

    avoiding infinte trigger loop

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- trigger to reset valid_email when email is updated
+
+CREATE TRIGGER email_update BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF email != NEW.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END;
