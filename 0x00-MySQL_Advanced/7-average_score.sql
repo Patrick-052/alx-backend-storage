66c8b783db5976f89b11ca0af1b68caf60f72b42
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-20 20:41:29 +0300

    Average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,9 @@
+-- procure that computes and stores average score for a student
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    SELECT AVG(score) INTO @average_score FROM corrections WHERE user_id = user_id;
+    UPDATE users SET average_score = @average_score WHERE id = user_id;
+END;$$
+DELIMITER ;
