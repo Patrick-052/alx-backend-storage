9061f04deb51220895aecf4888b528580ed688c5
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-21 00:05:12 +0300

    Email trigger

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,11 @@
 -- trigger to reset valid_email when email is updated
 
+DELIMITER $$
 CREATE TRIGGER email_update BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
     SET NEW.valid_email = 0;
     END IF;
-END;
+END;$$
+DELIMITER ;
