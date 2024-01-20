2978b6bbb337d930917faf8f025713dcea38b7ca
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-20 21:39:49 +0300

    Index on more than one key

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- Creating an index on first letters of name and score in table names
 -- Advantage: you can filter on both columns using this index thus making the query faster
 
-CREATE INDEX idx_name_score_first ON names (name(1), score);
+CREATE INDEX idx_name_score_first IF NOT EXISTS ON names (name(1), score);
