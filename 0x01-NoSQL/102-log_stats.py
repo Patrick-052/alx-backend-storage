37884553e1cffc47e3351037ae659cc93ac9795c
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-24 00:51:38 +0300

    Stored nginx log stats

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -3,29 +3,25 @@
 from pymongo import MongoClient
 
 
-def nginx_logs(mongo_collection):
-    """ providing nginx stored logs """
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+if __name__ == "__main__":
+    """ Provides log stats, IP addresses and their count """
+    nginx = MongoClient(host='localhost', port=27017).logs.nginx
 
-    print(f"{mongo_collection.estimated_document_count()} logs")
+    print(f"{nginx.estimated_document_count()} logs")
     print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for i in methods:
-        count = mongo_collection.count_documents({'method': i})
+        count = nginx.count_documents({'method': i})
         print(f"\tmethod {i}: {count}")
 
-    print(f"{mongo_collection.count_documents({'path': '/status'})}" +
+    print(f"{nginx.count_documents({'path': '/status'})}" +
           " status check")
 
     print("IPs:")
-    ip_counts = mongo_collection.aggregate([
+    ip_counts = nginx.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
     ])
     for doc in ip_counts:
         print(f"{doc['_id']}: {doc['count']}")
-
-
-if __name__ == "__main__":
-    nginx = MongoClient(host='localhost', port=27017).logs.nginx
-    nginx_logs(nginx)
